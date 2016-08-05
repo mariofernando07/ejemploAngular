ae90383862437f6df86d9f77cf3bfac06028131a
Mario Riascos <mariofernando07@hotmail.com>
2016-08-05 16:15:21 -0500

    conexion api - incompleta

--- app.ts
+++ app.ts
@@ -1,15 +1,51 @@
 import { bootstrap } from "@angular/platform-browser-dynamic";
-import { Component } from "@angular/core";
+import {Component, Inject} from "@angular/core";
 import {disableDeprecatedForms, provideForms, NgForm} from "@angular/forms";
+import { HTTP_PROVIDERS } from '@angular/http';
+import "rxjs/add/operator/map";
+
+// Importar objetos de la librería http
+import { Http, Response, RequestOptions, Headers } from '@angular/http';
+// Importar la clase Observable desde la librería rxjs
+import { Observable }     from 'rxjs/Observable';
 @Component({
     selector: "init-app",
     templateUrl :"template/layout.html",
     providers: [NgForm]
 })
 export class AppComponent {
-    guardar(form: NgForm){
-        console.log("Guardando la empresa de salud -> " + JSON.stringify(form.value));
+    private urlBase: string;
+
+    constructor(@Inject(Http) private http: Http) {
+        this.urlBase = "http://localhost:8080/api/entidad";
     }
+
+    guardar(form: NgForm) {
+        let body = JSON.stringify(form.value);
+        console.log("Guardando la empresa de salud -> " + body);
+        /*
+        let headers = new Headers({ 'Content-Type': 'application/json' });
+        let options = new RequestOptions({ headers: headers });
+*/
+        this.pushEntidad(body).subscribe(
+            res=> console.log("success" + res),
+            err=> console.log("error -> " + err),
+            ()=> console.log("Finished")
+        );
+    }
+
+    pushEntidad(body)  {
+        console.log("pushEntidad -> " + body);
+        let headers = new Headers({ 'Content-Type': 'application/x-www-form-urlencoded' });
+        let options = new RequestOptions({ headers: headers,  method: "post" });
+        return this.http.post(`${this.urlBase}`, body, options)
+            .map(res =>{console.log("post sucess"); res.json()});
+
+    }
+
 }
 
-bootstrap(AppComponent, [disableDeprecatedForms(), provideForms()]);
+
+
+
+bootstrap(AppComponent, [HTTP_PROVIDERS, disableDeprecatedForms(), provideForms()]);
