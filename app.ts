1334464eb8d302f036b42bb0e68e62177cc06f58
Mario Riascos <mariofernando07@hotmail.com>
2016-08-05 17:13:06 -0500

    conexion api - completa (duplicados)

--- app.ts
+++ app.ts
@@ -3,11 +3,11 @@ import {Component, Inject} from "@angular/core";
 import {disableDeprecatedForms, provideForms, NgForm} from "@angular/forms";
 import { HTTP_PROVIDERS } from '@angular/http';
 import "rxjs/add/operator/map";
-
 // Importar objetos de la librería http
 import { Http, Response, RequestOptions, Headers } from '@angular/http';
 // Importar la clase Observable desde la librería rxjs
 import { Observable }     from 'rxjs/Observable';
+
 @Component({
     selector: "init-app",
     templateUrl :"template/layout.html",
@@ -22,30 +22,22 @@ export class AppComponent {
 
     guardar(form: NgForm) {
         let body = JSON.stringify(form.value);
-        console.log("Guardando la empresa de salud -> " + body);
-        /*
-        let headers = new Headers({ 'Content-Type': 'application/json' });
-        let options = new RequestOptions({ headers: headers });
-*/
         this.pushEntidad(body).subscribe(
-            res=> console.log("success" + res),
-            err=> console.log("error -> " + err),
+            res=> console.log("Success  pushEntidad"),
+            err=> console.log("Error -> " + err),
             ()=> console.log("Finished")
         );
     }
 
     pushEntidad(body)  {
-        console.log("pushEntidad -> " + body);
-        let headers = new Headers({ 'Content-Type': 'application/x-www-form-urlencoded' });
+        console.log("Guardando -> " + body);
+        //'application/x-www-form-urlencoded'
+        let headers = new Headers({ 'Content-Type': 'application/json' });
         let options = new RequestOptions({ headers: headers,  method: "post" });
         return this.http.post(`${this.urlBase}`, body, options)
-            .map(res =>{console.log("post sucess"); res.json()});
-
+            .map(res =>{console.log("Post Success");res.json()});
     }
 
 }
-
-
-
 
 bootstrap(AppComponent, [HTTP_PROVIDERS, disableDeprecatedForms(), provideForms()]);
